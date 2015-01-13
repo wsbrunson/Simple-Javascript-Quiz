f63655c05dfeddcc46090365c6a159a03831ae0f
Shane Brunson <brunson.ws@gmail.com>
2015-01-13 11:59:55 -0500

    fixed tallyScore()
    It was checking the id #c[indexQuestion]-[indexChoices] which was applied to the label of the radio buttons. added a new id #radio-c[indexQuestion]-[indexChoices] and applied to the the input tag, changed tallyScore to check the new id.

--- scripts/app.js
+++ scripts/app.js
@@ -1,62 +1,61 @@
-
-var allQuestions = [{question: "Question 1: Grand Central Terminal, Park Avenue, New York is the world's", 
-                     choices: ["A: largest railway station", 
-                               "B: highest railway station", 
-                               "C: longest railway station", 
+var allQuestions = [{question: "Question 1: Grand Central Terminal, Park Avenue, New York is the world's",
+                     choices: ["A: largest railway station",
+                               "B: highest railway station",
+                               "C: longest railway station",
                                "D: None of the above"],
                      correctAnswer: 0
-                    }, 
-                    {question: "Question 2: Entomology is the science that studies", 
-                     choices: ["A: Behavior of human beings", 
-                               "B: Insects", 
-                               "C: The origin and history of technical and scientific terms", 
+                    },
+                    {question: "Question 2: Entomology is the science that studies",
+                     choices: ["A: Behavior of human beings",
+                               "B: Insects",
+                               "C: The origin and history of technical and scientific terms",
                                "D: The formation of rocks"],
                      correctAnswer: 1
-                    }, 
-                    {question: "Question 3: Friction can be reduced by changing from", 
-                     choices: ["A: sliding to rolling", 
-                               "B: rolling to sliding", 
-                               "C: potential energy to kinetic energy", 
+                    },
+                    {question: "Question 3: Friction can be reduced by changing from",
+                     choices: ["A: sliding to rolling",
+                               "B: rolling to sliding",
+                               "C: potential energy to kinetic energy",
                                "D: dynamic to static"],
                      correctAnswer: 0
                     },
-                    {question: "Question 4: For seeing objects at the surface of water from a submarine under water, the instrument used is", 
-                     choices: ["A: kaleidoscope", 
-                               "B: spectroscope", 
-                               "C: periscope", 
+                    {question: "Question 4: For seeing objects at the surface of water from a submarine under water, the instrument used is",
+                     choices: ["A: kaleidoscope",
+                               "B: spectroscope",
+                               "C: periscope",
                                "D: telescope"],
                      correctAnswer: 2
                     },
-                    {question: "Question 5: Galileo was an Italian astronomer who", 
-                     choices: ["A: developed the telescope", 
-                               "B: discovered four satellites of Jupiter", 
-                               "C: discovered that the movement of pendulum produces a regular time measurement", 
+                    {question: "Question 5: Galileo was an Italian astronomer who",
+                     choices: ["A: developed the telescope",
+                               "B: discovered four satellites of Jupiter",
+                               "C: discovered that the movement of pendulum produces a regular time measurement",
                                "D: All of the above"],
                      correctAnswer: 3
                     },
-                    {question: "Question 6: Habeas Corpus Act 1679", 
-                     choices: ["A: states that no one was to be imprisoned without a writ or warrant stating the charge against him", 
-                               "B: provided facilities to a prisoner to obtain either speedy trial or release in bail", 
-                               "C: safeguarded the personal liberties of the people against arbitrary imprisonment by the king's orders", 
+                    {question: "Question 6: Habeas Corpus Act 1679",
+                     choices: ["A: states that no one was to be imprisoned without a writ or warrant stating the charge against him",
+                               "B: provided facilities to a prisoner to obtain either speedy trial or release in bail",
+                               "C: safeguarded the personal liberties of the people against arbitrary imprisonment by the king's orders",
                                "D: All of the above"],
                      correctAnswer: 3
                     },
-                    {question: "Question 7: For galvanizing iron which of the following metals is used?", 
-                     choices: ["A: Aluminium", 
-                               "B: Lead", 
-                               "C: Zinc", 
+                    {question: "Question 7: For galvanizing iron which of the following metals is used?",
+                     choices: ["A: Aluminium",
+                               "B: Lead",
+                               "C: Zinc",
                                "D: Copper"],
                      correctAnswer: 2
                     }
                     ];
-                    
+
 
 $(document).ready(function() {
-    
+
     var questionNavIndex = 0,
         numberOfCorrectAnswers = 0,
         allQuestionsArrayLength = allQuestions.length;
-    
+
     var printQuizQuestions = function() {
         for(var loopIndexQuestion = 0; loopIndexQuestion < allQuestionsArrayLength; loopIndexQuestion++) {
             var pathToQuestion = allQuestions[loopIndexQuestion].question,
@@ -66,71 +65,61 @@ $(document).ready(function() {
                 hastagDivElement = "#" + questionsDivElementID,
                 questionID = "q"+ loopIndexQuestion,
                 questionTag = "<p class='question' id='" + questionID + "'>" + pathToQuestion + "</div>";
-            
+
             $('#quiz').append(questionsDivElement);
             $(hastagDivElement).append(questionTag);
-            
+
             for(var loopIndexChoices = 0, x = pathToChoices.length; loopIndexChoices < x; loopIndexChoices++) {
                 var choicesID = "c" + loopIndexQuestion + "-" + loopIndexChoices,
-                    findAnswer = allQuestions[loopIndexQuestion].correctAnswer,                                             //This checks the correct answer
-                    answerID = "c" + loopIndexQuestion + "-" + findAnswer,                                                  //This checks the correct answer
                     choicesTag = "<label class='radio-label' id='" + choicesID + "'>" +
-                                 "<input type='radio' name='quiz-radio-button-" + loopIndexQuestion + 
-                                 "'>   " +  pathToChoices[loopIndexChoices] + "</label>",
-                    correctChoicesTag = "<label class='radio-label' id='" + choicesID + "'>" +                              //This checks the correct answer
-                                 "<input type='radio' checked='checked' name='quiz-radio-button-" + loopIndexQuestion + 
+                                 "<input type='radio' name='quiz-radio-button-" + loopIndexQuestion + "' id='radio-" + choicesID +
                                  "'>   " +  pathToChoices[loopIndexChoices] + "</label>";
-                
-                if(choicesID === answerID) {$(hastagDivElement).append(correctChoicesTag);}                                 //This checks the correct answer
-                else {$(hastagDivElement).append(choicesTag);}
+
+                $(hastagDivElement).append(choicesTag);
             }
-            
+
             if(loopIndexQuestion !==0) {$(hastagDivElement).hide();}
         }
     };
-    
+
     var tallyScore = function() {
         for(var i = 0; i < allQuestionsArrayLength; i++) {
             var findAnswer = allQuestions[i].correctAnswer,
-                answerID = "#c" + i + "-" + findAnswer;
-            
-            console.log(answerID);
-            console.log(answerID.getAttribute('checked'));
-            // Doesn't work right now: document.getElementById(answerID).checked
-            if(answerID.getAttribute('checked')) {
-                console.log('true');
+                answerID = "radio-c" + i + "-" + findAnswer;
+
+            //console.log(answerID);
+            //console.log(document.getElementById(answerID));
+            //console.log(document.getElementById(answerID).checked);
+            if(document.getElementById(answerID).checked) {
+                //console.log('true');
                 numberOfCorrectAnswers++;
-                console.log(numberOfCorrectAnswers);
+                //console.log(numberOfCorrectAnswers);
             };
         }
     };
 
-    printQuizQuestions();
-    tallyScore();
-    
     //Begin button
     $('#begin-button').click(function() {
         $('#welcome').hide();
         $('.nav-button').show();
-        
+
         if(questionNavIndex === 0) {
             $('#back-button').hide();
         }
-        
+
         printQuizQuestions();
-        tallyScore();
     });
-    
+
     //Next button
     $('#next-button').click(function() {
         var questionNavIDToHide = "#question-" + questionNavIndex;
-        
+
         $(questionNavIDToHide).hide();
-        
+
         questionNavIndex++
-        
+
         var questionNavIDToShow = "#question-" + questionNavIndex;
-        
+
         $(questionNavIDToShow).show();
 
         if(questionNavIndex >= allQuestionsArrayLength) {
@@ -139,28 +128,28 @@ $(document).ready(function() {
             tallyScore();
             $('#score').append("<p id='total'>Your total score is: " + numberOfCorrectAnswers + "</p>");
         }
-        
+
          if(questionNavIndex >= 0) {
             $('#back-button').show();
          }
     });
-    
+
     //Back button
     $('#back-button').click(function() {
         if(questionNavIndex === 0) {
             $('#back-button').hide();
         }
-        
+
         var questionNavIDToHide = "#question-" + questionNavIndex;
-        
+
         $(questionNavIDToHide).hide();
-        
+
         questionNavIndex--
-        
+
         var questionNavIDToShow = "#question-" + questionNavIndex;
-        
+
         $(questionNavIDToShow).show();
-        
+
         if(questionNavIndex <= allQuestionsArrayLength) {
             $('#next-button').show();
             $('#total').hide();
