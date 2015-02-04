312fa16015158b9d43da43971c3ee0b9342e74fd
Shane Brunson <brunson.ws@gmail.com>
2015-02-04 15:41:00 +0000

    optimized

--- src/app.js
+++ src/app.js
@@ -1,189 +1,116 @@
-/*
-var allQuestions = [{question: "Question 1: Grand Central Terminal, Park Avenue, New York is the world's",
-                     choices: [{answer: "A: largest railway station"},
-                               {answer: "B: highest railway station"},
-                               {answer: "C: longest railway station"},
-                               {answer: "D: None of the above"}],
-                     correctAnswer: 0
-                    },
-                    {question: "Question 2: Entomology is the science that studies",
-                     choices: [{answer: "A: Behavior of human beings"},
-                               {answer: "B: Insects"},
-                               {answer: "C: The origin and history of technical and scientific terms"},
-                               {answer: "D: The formation of rocks"}],
-                     correctAnswer: 1
-                    },
-                    {question: "Question 3: Friction can be reduced by changing from",
-                     choices: [{answer: "A: sliding to rolling"},
-                               {answer: "B: rolling to sliding"},
-                               {answer: "C: potential energy to kinetic energy"},
-                               {answer: "D: dynamic to static"}],
-                     correctAnswer: 0
-                    },
-                    {question: "Question 4: For seeing objects at the surface of water from a submarine under water, the instrument used is",
-                     choices: [{answer: "A: kaleidoscope"},
-                               {answer: "B: spectroscope"},
-                               {answer: "C: periscope"}, 
-                               {answer: "D: telescope"}],
-                     correctAnswer: 2
-                    },
-                    {question: "Question 5: Galileo was an Italian astronomer who",
-                     choices: [{answer: "A: developed the telescope"},
-                               {answer: "B: discovered four satellites of Jupiter"},
-                               {answer: "C: discovered that the movement of pendulum produces a regular time measurement"},
-                               {answer: "D: All of the above"}],
-                     correctAnswer: 3
-                    },
-                    {question: "Question 6: Habeas Corpus Act 1679",
-                     choices: [{answer: "A: states that no one was to be imprisoned without a writ or warrant stating the charge against him"},
-                               {answer: "B: provided facilities to a prisoner to obtain either speedy trial or release in bail"},
-                               {answer: "C: safeguarded the personal liberties of the people against arbitrary imprisonment by the king's orders"},
-                               {answer: "D: All of the above"}],
-                     correctAnswer: 3
-                    },
-                    {question: "Question 7: For galvanizing iron which of the following metals is used?",
-                     choices: [{answer: "A: Aluminium"},
-                               {answer: "B: Lead"},
-                               {answer: "C: Zinc"},
-                               {answer: "D: Copper"}],
-                     correctAnswer: 2
-                    }
-                    ];
-*/
-var printQuizQuestions = function() {
-        $.getJSON("src/questions.json", function(data) {
-        
-        allQuestionsArrayLength = data.questions.length;
+var previousQuizAttempts = [],
+    arrayOfCorrectAnswers = [],
+    allQuestions = {};
     
-        for(var loopIndexQuestion in data.questions) {
-            var pathToQuestions = data.questions[loopIndexQuestion];
-                
-            pathToQuestions.questionsDivElementID = "question-" + loopIndexQuestion;
-            pathToQuestions.questionID = "q"+ loopIndexQuestion;
+$.ajax({
+    url: "https://api.myjson.com/bins/2i86j",
+    async: false,
+    dataType: 'json',
+	success: function(data) {
+		allQuestions = data.questions;
+	}
+});
 
-            for(var loopIndexChoices in pathToQuestions.choices) {
-                var pathToChoices = pathToQuestions.choices[loopIndexChoices];
+var arrayLoop = function(array, task) {
+    for(var i = 0; i < array.length; i++) {
+        task(array[i], i);
+    }
+};
+
+// ----Print Questions Function ----
+// Prints questions by adding tag IDs to the questions.json file. Then uses Handlebars to
+// add the questions, choices, and IDs to the DOM
+
+var printQuizQuestions = function() {
+    arrayLoop(allQuestions, function(question, questionIndex) {
+        arrayOfCorrectAnswers.push(question.correctAnswer);  //used to create the id of correct answers
                 
-                pathToChoices.choiceID = "c" + loopIndexQuestion + "-" + loopIndexChoices;
-                pathToChoices.radioButtonGroup = "quiz-radio-button-" + loopIndexQuestion;
-                pathToChoices.radioID = "radio-" + pathToChoices.choiceID;
-            }
-        }
-        
-        var theTemplateScript = $("#quiz-template").html();
-        var theTemplate = Handlebars.compile(theTemplateScript);
-        $(".quiz").append(theTemplate(data.questions));
-    
+        // --- The rest of this arrayLoop creates id properties for each question
+        // --- so that the Handlebars template can construct each question
+        question.questionsDivElementID = "question-" + questionIndex;
+        question.questionID = "q"+ questionIndex;
+                
+        arrayLoop(question.choices, function(choice, choiceIndex) {
+            choice.choiceID = "c" + questionIndex + "-" + choiceIndex;
+            choice.radioButtonGroup = "quiz-radio-button-" + questionIndex;
+            choice.radioID = "radio-" + choice.choiceID;
         });
+    });
         
-    };
-    
-printQuizQuestions();
+    var theTemplateScript = $("#quiz-template").html();
+    var theTemplate = Handlebars.compile(theTemplateScript);
+    $(".quiz").append(theTemplate(allQuestions));
+};
+
+
 
 $(document).ready(function() {
-
     var questionNavIndex = 0,
         numberOfCorrectAnswers = 0,
-        previousQuizAttempts = [],
-        allQuestions = [],
-        allQuestionsArrayLength;
-    
-    // ----Print Questions Function ----
-    // Prints questions by adding tag IDs to the allQuestions array. Then uses Handlebars to
-    // add the questions, choices, and IDs to the DOM
-    
-    /*var printQuizQuestions = function() {
-        $.getJSON("src/questions.json", function(data) {
-        
-        allQuestionsArrayLength = data.questions.length;
-    
-        for(var loopIndexQuestion in data.questions) {
-            var pathToQuestions = data.questions[loopIndexQuestion];
-                
-            pathToQuestions.questionsDivElementID = "question-" + loopIndexQuestion;
-            pathToQuestions.questionID = "q"+ loopIndexQuestion;
+        correctAnswerIDArray = [],
+        allQuestionsArrayLength = allQuestions.length;
 
-            for(var loopIndexChoices in pathToQuestions.choices) {
-                var pathToChoices = pathToQuestions.choices[loopIndexChoices];
-                
-                pathToChoices.choiceID = "c" + loopIndexQuestion + "-" + loopIndexChoices;
-                pathToChoices.radioButtonGroup = "quiz-radio-button-" + loopIndexQuestion;
-                pathToChoices.radioID = "radio-" + pathToChoices.choiceID;
-            }
-        }
-        
-        var theTemplateScript = $("#quiz-template").html();
-        var theTemplate = Handlebars.compile(theTemplateScript);
-        $(".quiz").append(theTemplate(data.questions));
-    
-        });
-        
-    }; */
-    
     // ---- Tally Score ----
     // Creates the ID of the correct question for each radio-button-group, then finds out
     // if that radio button has been checked
     
     var tallyScore = function() {
-        for(var i = 0; i < allQuestionsArrayLength; i++) {
-            var findAnswer = allQuestions[i].correctAnswer,
-                answerID = "radio-c" + i + "-" + findAnswer;
+        arrayLoop(allQuestions, function(question, questionIndex) {
+            correctAnswerIDArray.push("radio-c" + questionIndex  + "-" + question.correctAnswer);
 
-            if(document.getElementById(answerID).checked) {numberOfCorrectAnswers++;}
-        }
+            if(document.getElementById(correctAnswerIDArray[questionIndex]).checked) {numberOfCorrectAnswers++;}
+        });
 
         $('#score-total').html(numberOfCorrectAnswers);
     };
     
     var updateProgressBar = function() {
-        var value = 100 / (allQuestionsArrayLength / questionNavIndex)
+        var value = 100 / (allQuestionsArrayLength / questionNavIndex);
         $('.progress-bar').css('width', value+'%').attr('aria-valuenow', value);
     };
     
     // ----Hide & Show elements----
-    var changeDisplayOfElements = function(elem, state, classTrue) {
-        var modifier;
+    var hideElement = function(elem, classTrue) {
+        var modifier = classTrue ? "." : "#",
+            element = modifier + elem;
         
-        if (classTrue) {modifier = ".";}
-        else {modifier = "#";}
-        var element = modifier + elem;
+        $(element).hide();
+    };
+    
+    var showElement = function(elem, classTrue) {
+        var modifier = classTrue ? "." : "#",
+            element = modifier + elem;
         
-        if(state === 'hide') {$(element).hide(); console.log('hide ' + element);}
-        else if(state === 'show') {$(element).show(); console.log('show ' + element);}
-        else {
-            console.log("changeDisplayOfElements state or elem is incorrect");
-            console.log(elem);
-            console.log(state);
-        }
+        $(element).show();
     };
     
     // ----Nav Button Function----
     var navigateQuestions = function(direction) {
         var questionNavIDToHide = "question-" + questionNavIndex;
-        changeDisplayOfElements(questionNavIDToHide, 'hide');
+        hideElement(questionNavIDToHide);
 
-        if(direction === "next") {questionNavIndex++;}
-        else if(direction === "back") {questionNavIndex--;}
+        if(direction === "next") { questionNavIndex++; }
+        else if(direction === "back") { questionNavIndex--; }
         else {
             console.log("navigateQuestions() recieved unexpexted input - use next or back");
             console.log(direction);
         }
 
         var questionNavIDToShow = "question-" + questionNavIndex;
-        changeDisplayOfElements(questionNavIDToShow, 'show');
-    }
+        showElement(questionNavIDToShow);
+    };
 
     // ----Begin button----
     $('#begin-button').click(function() {
-        changeDisplayOfElements('welcome', 'hide');
-        changeDisplayOfElements('nav-button', 'show', true);
-
+        hideElement('welcome');
+        showElement('nav-button', true);
+        printQuizQuestions();
         if(questionNavIndex === 0) {
-            changeDisplayOfElements('back-button', 'hide');
+            hideElement('back-button');
         }
 
-        changeDisplayOfElements('question-0', 'show');
-        changeDisplayOfElements('progress', 'show', true)
+        showElement('question-0');
+        showElement('progress', true);
     });
 
     // ----Next button----
@@ -192,15 +119,15 @@ $(document).ready(function() {
         updateProgressBar();
         
         if(questionNavIndex >= 0) {
-            changeDisplayOfElements('back-button', 'show');
+            showElement('back-button');
          }
         
         if(questionNavIndex === allQuestionsArrayLength) {
-            changeDisplayOfElements('nav-button', 'hide', true);
+            hideElement('nav-button', true);
             
             tallyScore();
             
-            changeDisplayOfElements('score', 'show')
+            showElement('score');
         }
     });
 
@@ -210,7 +137,7 @@ $(document).ready(function() {
         updateProgressBar();
         
         if(questionNavIndex === 0) {
-            changeDisplayOfElements('back-button', 'hide');
+            hideElement('back-button');
         }
     });
 
@@ -219,40 +146,40 @@ $(document).ready(function() {
         $('.question-container').each(function () {
             $(this).remove();
         });
-        questionNavIndex = 0
+        questionNavIndex = 0;
         previousQuizAttempts.push(numberOfCorrectAnswers);
         numberOfCorrectAnswers = 0;
         updateProgressBar();
-        changeDisplayOfElements('score', 'hide');
-        changeDisplayOfElements('progress', 'hide', true);
-        changeDisplayOfElements('welcome', 'show');
+        hideElement('score');
+        hideElement('progress', true);
+        showElement('welcome');
+        printQuizQuestions();
     });
     
     // ----Show Answers button----
     $('#show-button').click(function() {
-        for(var i = 0; i < allQuestionsArrayLength; i++) {
-            for(var j = 0; j < allQuestions[i].choices.length; j++) {
-                var findAnswer = allQuestions[i].correctAnswer,
-                answerID = "radio-c" + i + "-" + findAnswer,
-                labelID = "#c" + i + "-" + j,
-                radioID = "radio-c" + i + "-" + j,
-                questionContainer = "question-" + i;
+        arrayLoop(allQuestions, function(question, questionIndex) {
+            arrayLoop(question.choices, function(choice, choiceIndex) {
+               var hastageChoiceID = "#" + choice.choiceID;
                 
-                if(document.getElementById(radioID).checked & !(answerID === radioID)) {
-                    $(labelID).addClass('highlight-incorrect');
-                    $(questionContainer).addClass('question-incorrect');
+                if(document.getElementById(choice.radioID).checked && correctAnswerIDArray[questionIndex] !== choice.radioID) {
+                    $(hastageChoiceID).addClass('highlight-incorrect');
+                    $(question.questionsDivElementID).addClass('question-incorrect');
+               }
+               
+                else if(correctAnswerIDArray[questionIndex] === choice.radioID) {
+                    $(hastageChoiceID).addClass('highlight-correct');
                 }
-                else if(answerID === radioID) {
-                    $(labelID).addClass('highlight-correct');
-                }
-            }
-        }
-        
-        for(i = 0; i < allQuestionsArrayLength; i++) {
-            var element = 'question-' + i;
-            changeDisplayOfElements(element, 'show');
+            });
+        });
+
+        arrayLoop(allQuestions, function(question, questionIndex) {
+            var element = 'question-' + questionIndex;
+            showElement(element);
             $('label').children().hide();
-        }
+        });
+        
+        hideElement('progress', true);
     });
 });
 
